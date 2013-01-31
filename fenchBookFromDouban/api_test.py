ffd095410eda28255be710f8a0bad354a2c39d9c
hopex <p85015937_player@163.com>
2013-02-01 00:01:14 +0800

    rm patch fench....

--- fenchBookFromDouban/api_test.py
+++
@@ -1,9 +0,0 @@
-import httplib2
-httplib2.debuglevel = 1
-h = httplib2.Http('.cache')
-resp, content = h.request('https://api.douban.com/v2/book/1220562')
-print(resp.status)
-print(content)
-# print(len(content))
-# print(resp.fromcache)
-# print(dict(resp.items()))
